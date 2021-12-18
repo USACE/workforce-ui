125d3d9069ddd890c6e8740f9e5168354833911b
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2021-12-18 13:32:16 -0500

    common save and cancel buttons

--- src/app-components/position-tables/EditPositionModal.js
+++ src/app-components/position-tables/EditPositionModal.js
@@ -5,6 +5,7 @@ import { PencilAltIcon } from '@heroicons/react/outline';
 import { connect } from 'redux-bundler-react';
 import Select from 'react-select';
 import ReactTooltip from 'react-tooltip';
+import { SaveButton, CancelButton } from '../forms/buttons';
 
 const EditPositionModal = connect(
   'doModalClose',
@@ -322,22 +323,14 @@ const EditPositionModal = connect(
             </div>
           </div>
           <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button
-              type="button"
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={handleSubmit}
-            >
-              Submit
-            </button>
-            <button
-              type="button"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            <SaveButton label="Submit" onClick={handleSubmit} />
+
+            <CancelButton
+              label="Cancel"
               onClick={() => {
                 doModalClose();
               }}
-            >
-              Cancel
-            </button>
+            />
           </div>
         </div>
       </Transition>
